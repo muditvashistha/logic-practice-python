48cc089efb983c555ec297378963732981a4688b
Mudit Vashistha <154427328+muditvashistha@users.noreply.github.com>
2025-05-07 23:53:23 +0530

    Add files via upload

---
+++ Number of Good Pairs.py
@@ -0,0 +1,36 @@
+# Given an array of integers nums, return the number of good pairs.
+
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+# Example 2:
+
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+# Example 3:
+
+# Input: nums = [1,2,3]
+# Output: 0
+
+from collections import Counter
+
+
+nums=[1,2,3,1,1,3]
+check=Counter(nums)
+pairs=0
+
+print(check)
+
+for index,element in enumerate(nums):
+    check[element]-=1
+    pairs+=check[element]
+
+print(pairs)
+
