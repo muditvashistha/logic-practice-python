3a930d93f68514833a4c6a8c53e4e710953ca3cf
Mudit Vashistha <154427328+muditvashistha@users.noreply.github.com>
2025-05-11 18:20:44 +0530

    Add files via upload

---
+++ Asteroid Collision.py
@@ -0,0 +1,56 @@
+# We are given an array asteroids of integers representing asteroids in a row. The indices of the asteriod in the array represent their relative position in space.
+
+# For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+# Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+# Example 1:
+
+# Input: asteroids = [5,10,-5]
+# Output: [5,10]
+# Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+# Example 2:
+
+# Input: asteroids = [8,-8]
+# Output: []
+# Explanation: The 8 and -8 collide exploding each other.
+# Example 3:
+
+# Input: asteroids = [10,2,-5]
+# Output: [10]
+# Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+
+
+asteroids=[10,2,-5]
+
+
+length=len(asteroids)
+
+for i in range(length-1,-1,-1):
+    if i==0:
+        continue
+    if len(asteroids)==1:
+        break
+    if i>=len(asteroids):
+        i=len(asteroids)-1
+
+    if asteroids[i] < 0 and asteroids[i-1] > 0: #found a case of asteroids appraoching each other
+        left=asteroids[i-1]
+        right=asteroids[i]
+        if abs(right) > abs(left):
+            asteroids.remove(left)
+            i=min(i,len(asteroids)-1)
+            i=i+1
+        elif abs(right) == abs(left):
+            asteroids.remove(right), asteroids.remove(left)
+            i=min(i,len(asteroids)-1)
+            i=i+1
+        else:
+            asteroids.remove(right)
+        
+    else:
+        break
+
+print(asteroids)
