17e4d6fb20adfbbd4ff64b2cb2e9d02b8fbb9279
Mudit Vashistha <154427328+muditvashistha@users.noreply.github.com>
2025-09-05 12:21:41 +0530

    Add files via upload

---
+++ Third Maximum Number.py
@@ -0,0 +1,53 @@
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,1]
+# Output: 1
+# Explanation:
+# The first distinct maximum is 3.
+# The second distinct maximum is 2.
+# The third distinct maximum is 1.
+# Example 2:
+
+# Input: nums = [1,2]
+# Output: 2
+# Explanation:
+# The first distinct maximum is 2.
+# The second distinct maximum is 1.
+# The third distinct maximum does not exist, so the maximum (2) is returned instead.
+# Example 3:
+
+# Input: nums = [2,2,3,1]
+# Output: 1
+# Explanation:
+# The first distinct maximum is 3.
+# The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+# The third distinct maximum is 1.
+
+
+class Solution(object):
+    def thirdMax(self, nums):
+        arr_sorted = sorted(nums, reverse=True)
+        sorted_set=set(arr_sorted)
+        check_list=sorted(sorted_set,reverse=True)
+
+        num=0
+        count=0
+
+        if len(check_list) < 3:
+            return check_list[0]
+        else:
+            for i in check_list:
+                if count == 3:
+                    break
+                else:
+                    num = i
+                    count = count + 1   
+            return num
+
+  
+    
+
