3c1ed2a95f5963c5f3030d0ec1c83768e8b705e6
Mudit Vashistha <154427328+muditvashistha@users.noreply.github.com>
2025-09-14 11:47:52 +0530

    Add files via upload

---
+++ Missing number.py
@@ -0,0 +1,61 @@
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,0,1]
+
+# Output: 2
+
+# Explanation:
+
+# n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+# Example 2:
+
+# Input: nums = [0,1]
+
+# Output: 2
+
+# Explanation:
+
+# n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+# Example 3:
+
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+
+# Output: 8
+
+# Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+
+
+# nums = [0,1]
+# nums_sorted=sorted(nums)
+# print(nums_sorted)
+# check_list=[]
+# for i in range(len(nums_sorted) + 1):
+#     check_list.append(i)
+
+# print(check_list)
+
+# for i , j in zip(nums_sorted , check_list):
+#     if i == j:
+#         continue
+#     else:
+#         print(j)
+#         break
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)+1):
+            if i in set(nums):
+                continue
+            else:
+                return i
+
+
+
