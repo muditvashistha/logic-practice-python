8ddbf2728fc24cc29cfd44fa12258381c9e56093
Mudit Vashistha <154427328+muditvashistha@users.noreply.github.com>
2025-12-01 23:49:41 +0530

    Add files via upload

---
+++ Subarray Sum Equals K.py
@@ -0,0 +1,30 @@
+# Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1], k = 2
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,2,3], k = 3
+# Output: 2
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        map = {0:1}
+        
+        prefix = 0
+        c = 0
+        for i in nums:
+            prefix = prefix + i
+            if (prefix-k) in map:
+                c = c + map[prefix-k]
+            if prefix in map:
+                map[prefix] = map[prefix] + 1
+            else:
+                map[prefix] = 1
+        return c
